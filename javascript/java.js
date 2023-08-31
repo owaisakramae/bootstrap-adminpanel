9a5ed48a30eb31588c4deff07fd8fc099aac2d68
Owais <r.owaisakram@gmail.com>
2023-08-31 15:02:02 +0500

    submenu

--- javascript/java.js
+++ javascript/java.js
@@ -13,8 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
         menuToggle.innerHTML = '<i class="fas fa-bars"></i>';
       }
     });
-});
 
+    $('.sub-menu ul').hide();
+    $(".sub-menu a").click(function(){
+  $(this).parent(".sub-menu").children("ul").slideToggle("100");
+  $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  });
+});
 
 
 
